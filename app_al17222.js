90e3f9c9f1f05f10cace2aa9841fe013b72b0661
Ana Sofía Mascareño Morales <al17222@cbtis051.edu.mx>
2019-11-26 14:27:26 -0700

    Commit inicial del server

--- app_al17222.js
+++ app_al17222.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GOT - Mascareño Morales Ana Sofía\n');
+    res.end('Hola mundo Node JS Repo GIT - Mascareño Morales Ana Sofía\n');
 }).listen(8081);
 console.log('Server running on port 8081');
