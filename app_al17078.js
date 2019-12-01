824fc28ff6ba813e3698107b465c42623f35f855
Santos Hernández Lidia del Carmen <al17078@cbtis051.edu.mx>
2019-12-01 12:44:26 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17078.js
+++ app_al17078.js
@@ -1,9 +1,15 @@
 var http = require('http');
-http.createServer(function (req, res){
+http.createServer(function (req, res) {
+
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Santos Hernández Lidia del Carmen\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Santos Hernández Lidia del Carmen\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+
 }).listen(8081);
 console.log('Server running on port 8081');
-
-
