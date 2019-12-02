950fe0b20467d870071fb39980c0fc3e8a57dce2
Jose Angel Silva Zatarain <al17664@cbtis051.edu.mx>
2019-12-02 10:32:17 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17664.js
+++ app_al17664.js
@@ -1,7 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res){
+var http = require ('http');
+http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Jose Angel Silva Zatarain\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo Git - Silva Zatarain Jose Angel\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+
 }).listen(8081);
 console.log('Server running on port 8081');
